b5762aee2931bd20313341d33ded86ef08b3b8b1
atla <atlamasthanaiah@gmail.com>
2018-07-19 11:35:10 +0530

    new

--- app/src/main/java/akshay/mybrowser/MainActivity.kt
+++ app/src/main/java/akshay/mybrowser/MainActivity.kt
@@ -1,17 +1,37 @@
 package akshay.mybrowser
 
+import android.app.ProgressDialog
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var pd : ProgressDialog? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        wv.webViewClient = WebViewClient()
+        wv.settings.javaScriptEnabled = true
+        pd = ProgressDialog(this)
+        pd!!.setMessage("Please Wait")
+        wv.webViewClient = object:  WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                pd!!.show()
+                pd.setCanceledOnTouchOutside(false)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                pd!!.dismiss()
+            }
+        }
     }
 
     fun load(v:View){
